6c4f845f92aff80b22c31913ae215ebe4665bf2e
Alex Kutzke <alexkutzke@gmail.com>
2026-03-05 08:06:28 -0300

    Revert "bug: force error"
    This reverts commit 12258a0f886890e4ae124c83f0e1a4a30aeab415.

--- conta.py
+++ conta.py
@@ -9,7 +9,7 @@ class ContaBancaria:
         return self.saldo
 
     def sacar(self, valor):
-        if valor > 0:
+        if valor > 0 and self.saldo >= valor:
             self.saldo -= valor
             return True
         return False
